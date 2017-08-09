e60ef68c5ae3ff60d4e39fade96fb31b3d892d34
Jordan Tucker <jordanbtucker@gmail.com>
2017-08-09 17:24:25 -0500

    Fix spacing and lint

--- web/build/dev-client.js
+++ web/build/dev-client.js
@@ -1,9 +1,10 @@
-/* eslint-disable */
 require('eventsource-polyfill')
+
+// eslint-disable-next-line node/no-missing-require
 const hotClient = require('webpack-hot-middleware/client?noInfo=true&reload=true')
 
 hotClient.subscribe(function (event) {
-  if (event.action === 'reload') {
-    window.location.reload()
-  }
+	if (event.action === 'reload') {
+		window.location.reload()
+	}
 })
